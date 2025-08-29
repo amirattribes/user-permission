1fbac338f62b45af2eba8f9403c85aa5c3f8a378
it.dev109 <it.dev109@dawateislami.net>
2025-08-29 18:36:39 +0500

    add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // // User seeding example
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'password' => '$2y$12$sE7RiUjbFdw8FoQaYaG1F.YU.eFRCfxF9PZ5JdGjsZnv1T6oamFGa' // click@123
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => '$2y$12$sE7RiUjbFdw8FoQaYaG1F.YU.eFRCfxF9PZ5JdGjsZnv1T6oamFGa' // click@123
+        ]);
 
         // Custom seeders ko yahan call karo
         $this->call([
